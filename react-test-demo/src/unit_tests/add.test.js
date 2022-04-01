ce701d26899a7a3db707d3e76c960d706a670a9a
Fred Jiang <jiangf@shinetechchina.com>
2022-04-02 07:05:30 +0800

    test

--- react-test-demo/src/unit_tests/add.test.js
+++ react-test-demo/src/unit_tests/add.test.js
@@ -1,11 +1,10 @@
 
 import add from './add';
 
-// 通过 test 函数创建一个测试用例
 test('renders learn react link', () => {
-  // 执行 add 函数得到结果
+  // act
   const s = add(1, 2);
   
-  // 使用 Jest 提供的 expect 函数断言结果等于3
+  // assert
   expect(s).toBe(3);
 });
