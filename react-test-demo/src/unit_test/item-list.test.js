f8099662fc75c79fd42003a3774eb7df06040111
Fred Jiang <jiangf@shinetechsoftware.com>
2022-04-02 05:47:48 +0800

    test

---
+++ react-test-demo/src/unit_test/item-list.test.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ItemList from './item-list';
+
+test('renders "no items" when the item list is empty', () => {
+    // arrange
+    const container = document.createElement('div');
+
+    //act
+    ReactDOM.render(<ItemList items={[]} />, container);
+
+    //assert
+    expect(container.textContent).toMatch('no items');
+});
+
+test('renders the items in a list', () => {
+    // arrange
+    const container = document.createElement('div');
+
+    // act
+    ReactDOM.render(<ItemList items={['apple', 'orange', 'pear']} />, container);
+
+    // assert
+    expect(container.textContent).toMatch('apple');
+    expect(container.textContent).toMatch('orange');
+    expect(container.textContent).toMatch('pear');
+});
