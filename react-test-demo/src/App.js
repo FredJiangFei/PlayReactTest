f0648edac73df51830b2abc61d98b5a35669eed1
Fred Jiang <jiangf@shinetechsoftware.com>
2022-04-02 09:36:00 +0800

    router test

--- react-test-demo/src/App.js
+++ react-test-demo/src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import * as React from 'react'
+import { Link, Route, Routes } from 'react-router-dom'
+import LocationDisplay from './components/LocationDisplay'
+
+const About = () => <div>You are on the about page</div>
+const Home = () => <div>You are home</div>
+const NoMatch = () => <div>No match</div>
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Link to="/">Home</Link>{' | '} 
+      <Link to="/about">About</Link>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={ <NoMatch /> } />
+      </Routes>
+      <LocationDisplay />
+    </>
   );
 }
 
---
+++ react-test-demo/src/components/LocationDisplay.js
@@ -0,0 +1,8 @@
+import { useLocation } from 'react-router-dom'
+
+const LocationDisplay = () => {
+    const location = useLocation()
+    return <div data-testid="location-display">path: {location.pathname}</div>
+}
+
+export default LocationDisplay;
