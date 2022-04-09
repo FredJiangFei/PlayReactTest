b41705d6430b4b02c4a074a1b63e7050c6b58a6c
Fred Jiang <jiangf@shinetechchina.com>
2022-04-09 17:10:13 +0800

    test

--- react-test-demo/src/__tests__/LoginForm.js
+++ react-test-demo/src/__tests__/LoginForm.js
@@ -1,38 +1,10 @@
 import ReactDOM from 'react-dom';
-import { render, Simulate, cleanup, fireEvent } from '@testing-library/react';
+import { render, cleanup, fireEvent } from '@testing-library/react';
 import LoginForm from '../components/LoginForm';
 
 afterEach(cleanup);
 
-describe.skip('test login', () => {
-  test('simple', () => {
-    // Arrange
-    const fakeUser = { username: 'fred', password: '123' };
-    const handleSubmit = jest.fn();
-
-    const div = document.createElement('div');
-    ReactDOM.render(<LoginForm onSubmit={handleSubmit} />, div);
-
-    const inputs = div.querySelectorAll('input');
-    const usernameNode = inputs[0];
-    const passwordNode = inputs[1];
-    const formNode = div.querySelector('form');
-    const submitButtonNode = div.querySelector('button');
-
-    usernameNode.value = fakeUser.username;
-    passwordNode.value = fakeUser.password;
-
-    // Act
-    const event = new Event('submit');
-    formNode.dispatchEvent(event);
-    fireEvent.click(getByText('submit'));
-
-    // Assert
-    expect(handleSubmit).toHaveBeenCalledTimes(1);
-    expect(handleSubmit).toHaveBeenCalledWith(fakeUser);
-    expect(submitButtonNode.type).toBe('submit');
-  });
-
+describe.only('test login', () => {
   test('with react-testing-library fireEvent', () => {
     // Arrange
     const fakeUser = { username: 'fred', password: '123' };
@@ -51,32 +23,6 @@ describe.skip('test login', () => {
 
     // Act
     fireEvent.click(getByText('Login'));
-
-    // Assert
-    expect(handleSubmit).toHaveBeenCalledTimes(1);
-    expect(handleSubmit).toHaveBeenCalledWith(fakeUser);
-    expect(submitButtonNode.type).toBe('submit');
-  });
-
-  test('with react-testing-library', () => {
-    // Arrange
-    const fakeUser = { username: 'fred', password: '123' };
-    const handleSubmit = jest.fn();
-
-    const { container, getByLabelText, getByText } = render(
-      <LoginForm onSubmit={handleSubmit} />
-    );
-
-    const usernameNode = getByLabelText('Username');
-    const passwordNode = getByLabelText('Password');
-    const formNode = container.querySelector('form');
-    const submitButtonNode = getByText('Login');
-
-    usernameNode.value = fakeUser.username;
-    passwordNode.value = fakeUser.password;
-
-    // Act
-    Simulate.submit(formNode);
 
     // Assert
     expect(handleSubmit).toHaveBeenCalledTimes(1);
