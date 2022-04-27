a8259db7b01b2b78c7c38666b26dc55f4ddbfc4f
Fred Jiang <jiangf@shinetechsoftware.com>
2022-04-27 10:27:55 +0800

    reducer test

--- react-test-demo/src/__tests__/Counter.js
+++ react-test-demo/src/__tests__/Counter.js
@@ -1,29 +1,38 @@
-import * as React from 'react'
-import { Provider } from 'react-redux'
-import { render, fireEvent, screen } from '@testing-library/react'
-import { Counter } from '../components/Counter'
-import store from '../store/Store'
+import * as React from 'react';
+import { Provider } from 'react-redux';
+import { render, fireEvent, screen } from '@testing-library/react';
+import { Counter } from '../components/Counter';
+import { combineReducers, createStore } from 'redux';
+import { CounterReducer } from '../store/reducers/counter.reducer';
 
 describe('counter redux', () => {
   test('increment 1 to be 1', () => {
+    const store = createStore(combineReducers({
+      counter: CounterReducer
+    }));
+
     render(
       <Provider store={store}>
         <Counter />
       </Provider>
-    )
+    );
 
-    fireEvent.click(screen.getByText('+'))
+    fireEvent.click(screen.getByText('+'));
 
-    expect(screen.getByLabelText(/count/i)).toHaveTextContent('1')
-  })
+    expect(screen.getByLabelText(/count/i)).toHaveTextContent('1');
+  });
 
   test('decrement 1 to be -1', () => {
+    const store = createStore(combineReducers({
+      counter: CounterReducer
+    }));
+
     render(
       <Provider store={store}>
         <Counter />
       </Provider>
-    )
-    fireEvent.click(screen.getByText('-'))
-    expect(screen.getByLabelText(/count/i)).toHaveTextContent('-1')
-  })
-})
+    );
+    fireEvent.click(screen.getByText('-'));
+    expect(screen.getByLabelText(/count/i)).toHaveTextContent('-1');
+  });
+});
